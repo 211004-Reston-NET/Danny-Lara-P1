88b795a19817a90c742bc59adae7feb6fbaabd8f
dlara2021 <dlara2021@gmail.com>
2021-10-05 16:01:04 -0400

    Added House class

---
+++ C#_Fundamentals/HelloWorld/House.cs
@@ -0,0 +1,16 @@
+namespace HouseFunction
+{
+    public class House
+    {
+        private string owner;
+        private string ghost;
+        private int roomCount;
+
+        public House()
+        {
+            owner = "Danny";
+            ghost = "Casper";
+            roomCount = 1;
+        }
+    }
+}
--- C#_Fundamentals/HelloWorld/Program.cs
+++ C#_Fundamentals/HelloWorld/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using HouseFunction;
+//Line comment example
+/*
+    Mutiple line comment example
+    - C# uses PascelCase for the most part
+    - We use camelCase for naming fields and variables
+*/
 
 namespace HelloWorld
 {
     class Program
     {
+        /*
+            - Main method is the starting point for a C# program
+            - static means: I don't have to instantiate the class to use the method/variable
+
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            House danny = new House();
         }
     }
 }
