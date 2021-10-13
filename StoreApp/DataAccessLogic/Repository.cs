a366fcfc96d9a44528bf4ec8d06a7517b7684dad
dlara2021 <dlara2021@gmail.com>
2021-10-13 17:33:04 -0400

    Added StoreInit method to create store repository

--- StoreApp/DataAccessLogic/Repository.cs
+++ StoreApp/DataAccessLogic/Repository.cs
@@ -11,6 +11,23 @@ namespace DataAccessLogic
         private const string _customerFilepath = "./../DataAccessLogic/Database/CustomerData.json";
         private string _jsonString;
 
+        public Repository()
+        {
+            if(!File.Exists(_storeFilepath))
+            {
+                FileStream f = File.Create(_storeFilepath);
+                f.Close();
+                File.WriteAllText(_storeFilepath, "[]");
+                StoreInit();
+            }
+            if(!File.Exists(_customerFilepath))
+            {
+                FileStream f = File.Create(_customerFilepath);
+                f.Close();
+                File.WriteAllText(_customerFilepath, "[]");
+            }
+        }
+
         public Store AddStore(Store p_store)
         {
             List<Store> storeList = GetAllStores();
@@ -40,6 +57,105 @@ namespace DataAccessLogic
             _jsonString = File.ReadAllText(_customerFilepath);
             return JsonSerializer.Deserialize<List<Customer>>(_jsonString);
         }
-        
+        static void StoreInit()
+        {
+            Repository storeWriter = new Repository();
+
+            Store s1 = new Store()
+            {
+                Name = "Rose's Roses",
+                Address = "321 Baltic Ave. Orlando, FL",
+                Products = new List<Product>()
+            };
+            Product p = new Product()
+            {
+                Name = "Roses (6)",
+                Price = 10.99,
+                Description = "Half a dozen roses.",
+                Category = "Flowers"
+            };
+            s1.Products.Add(p);
+            p = new Product()
+            {
+                Name = "Roses (12)",
+                Price = 19.99,
+                Description = "A dozen roses.",
+                Category = "Flowers"
+            };
+            s1.Products.Add(p);
+            storeWriter.AddStore(s1);
+
+            Store s2 = new Store()
+            {
+                Name = "David's Daffodils",
+                Address = "1600 Park Place Dr. Los Angeles, CA",
+                Products = new List<Product>()
+            };
+            p = new Product()
+            {
+                Name = "Daffodils (12)",
+                Price = 24.99,
+                Description = "A dozen daffodils.",
+                Category = "Flowers"
+            };
+            s2.Products.Add(p);
+            p = new Product()
+            {
+                Name = "Flower pot",
+                Price = 2.99,
+                Description = "A small flower pot",
+                Category = "Pots"
+            };
+            s2.Products.Add(p);
+            storeWriter.AddStore(s2);
+
+            Store s3 = new Store()
+            {
+                Name = "Lily's Lilies",
+                Address = "3 Privit Dr Little Winding, UK",
+                Products = new List<Product>()
+            };
+            p = new Product()
+            {
+                Name = "Lilies (20)",
+                Price = 69.99,
+                Description = "20 stems of lilies.",
+                Category = "Flowers"
+            };
+            s3.Products.Add(p);
+            p = new Product()
+            {
+                Name = "Fancy Vase",
+                Price = 74.99,
+                Description = "A hand-painted fancy vase.",
+                Category = "Vases"
+            };
+            s3.Products.Add(p);
+            storeWriter.AddStore(s3);
+
+            Store s4 = new Store()
+            {
+                Name = "Steve's Sunflowers",
+                Address = "722 Orchard Dr. Paso Robles, CA",
+                Products = new List<Product>()
+            };
+            p = new Product()
+            {
+                Name = "Sunflowers (12)",
+                Price = 12.99,
+                Description = "A dozen sunflowers",
+                Category = "Flowers"
+            };
+            s4.Products.Add(p);
+            p = new Product()
+            {
+                Name = "Roasted Sunflower Seeds",
+                Price = 1.50,
+                Description = "A 5.25oz bag of roasted sunflower seeds",
+                Category = "Snacks"
+            };
+            s4.Products.Add(p);
+            storeWriter.AddStore(s4);
+        }
     }
 }
--- StoreApp/UserInterface/Program.cs
+++ StoreApp/UserInterface/Program.cs
@@ -54,54 +54,5 @@ namespace UserInterface
             }
             
         }
-        static void StoreInit()
-        {
-            StoreBL storeWriter = new StoreBL(new Repository());
-
-            Store s1 = new Store();
-            s1.Name = "Rose's Roses";
-            s1.Address = "321 Baltic Ave. Orlando, FL";
-            List<Product> roseProducts = new List<Product>();
-            Product p = new Product();
-            p.Name = "Roses (6)";
-            p.Price = 10.99;
-            p.Description = "Half a dozen roses.";
-            p.Category = "Flowers";
-            roseProducts.Add(p);
-            p = new Product();
-            p.Name = "Roses (12)";
-            p.Price = 19.99;
-            p.Description = "A dozen roses.";
-            p.Category = "Flowers";
-            roseProducts.Add(p);
-            s1.Products = roseProducts;
-            storeWriter.Add(s1);
-
-            Store s2 = new Store();
-            s1.Name = "David's Daffodils";
-            s2.Address = "1600 Park Place Dr. Los Angeles, CA";
-            List<Product> daffList = new List<Product>();
-            p = new Product();
-            p.Name = "Daffodils (12)";
-            p.Price = 24.99;
-            p.Description = "A dozen daffodils.";
-            p.Category = "Flowers";
-            daffList.Add(p);
-            p = new Product();
-            p.Name = "Flower pot";
-            p.Price = 2.99;
-            p.Description = "A small flower pot";
-            p.Category = "Pots";
-            daffList.Add(p);
-            s2.Products = daffList;
-            storeWriter.Add(s2);
-
-            Store s3 = new Store();
-            s3.Name = "Lily's Lilies";
-            s3.Address = "3 Privit Dr Little Winding, UK";
-            List<Product> lilyProducts = new List<Product>();
-            p = new Product();
-            
-        }
     }
 }
