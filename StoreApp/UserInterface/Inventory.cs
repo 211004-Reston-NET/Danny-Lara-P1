963601d5f02c0bc26b5820e0c261849bcede53e4
dlara2021 <dlara2021@gmail.com>
2021-10-13 18:08:35 -0400

    Implemented view inventory functionality

--- StoreApp/UserInterface/Inventory.cs
+++ StoreApp/UserInterface/Inventory.cs
@@ -1,28 +1,51 @@
 using System;
-using DataAccessLogic;
+using System.Collections.Generic;
+using BusinessLogic;
+using Models;
 namespace UserInterface
 {
     public class Inventory : IMenu
     {
+        private StoreBL _storeBL = new StoreBL(new DataAccessLogic.Repository());
+        private List<Store> _stores;
         public MenuType Choice()
         {
+            _stores = _storeBL.GetAll();
+            List<Product> products;
+            Store store;
             string input = Console.ReadLine();
             switch (input)
             {
-                case "1":
-                    
-                    break;
-                case "2":
+                case "4":
+                    products = _stores[0].Products;
+                    store = _stores[0];
                     break;
                 case "3":
+                    products = _stores[1].Products;
+                    store = _stores[1];
                     break;
-                case "4":
+                case "2":
+                    products = _stores[2].Products;
+                    store = _stores[2];
                     break;
-                case "5":
+                case "1":
+                    products = _stores[3].Products;
+                    store = _stores[3];
                     break;
+                case "0":
+                    return MenuType.MainMenu;
                 default:
-                    break;
+                    Console.WriteLine("Invalid input!\nPress Enter to continue...");
+                    return MenuType.Inventory;
             }
+            Console.Clear();
+            Console.WriteLine($"Inventory for {store.Name}:\n");
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item + "\n");
+            }
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
             Console.Clear();
             return MenuType.MainMenu;
         }
@@ -31,11 +54,11 @@ namespace UserInterface
         {
             Console.Clear();
             Console.WriteLine("Which store would you like to see?");
-            Console.WriteLine("[1] - Rose's Roses");
-            Console.WriteLine("[2] - David's Daffodils");
-            Console.WriteLine("[3] - Lily's Lilies");
-            Console.WriteLine("[4] - Steve's Sunflowers");
-            Console.WriteLine("[5] - Go back to Main Menu");
+            Console.WriteLine("[4] - Rose's Roses");
+            Console.WriteLine("[3] - David's Daffodils");
+            Console.WriteLine("[2] - Lily's Lilies");
+            Console.WriteLine("[1] - Steve's Sunflowers");
+            Console.WriteLine("[0] - Go back to Main Menu");
         }
     }
 }
