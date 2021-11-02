4542248877205346ebf0f9164a6d0f1ec6117911
dlara2021 <dlara2021@gmail.com>
2021-11-02 14:17:12 -0400

    Updated method documentation

--- StoreApp/BusinessLogic/CustomerBL.cs
+++ StoreApp/BusinessLogic/CustomerBL.cs
@@ -12,6 +12,10 @@ namespace BusinessLogic
             _data = p_data;
         }
 
+        /// <summary>
+        /// Gets a list of all the customers in the database
+        /// </summary>
+        /// <returns>A list of all the customers in the customer table</returns>
         public List<Customer> GetAll()
         {
             List<Customer> customers = _data.GetAllCustomers();
@@ -29,6 +33,11 @@ namespace BusinessLogic
             }
             return customers;
         }
+        /// <summary>
+        /// Gets a specific customer by customer ID
+        /// </summary>
+        /// <param name="p_custId">The customer ID</param>
+        /// <returns>The specific customer from the customer table or null if the ID is not found in the table</returns>
         public Customer GetCustomer(int p_custId)
         {
             List<Customer> allCusts = _data.GetAllCustomers();
@@ -39,6 +48,11 @@ namespace BusinessLogic
             }
             return null;
         }
+        /// <summary>
+        /// Add a customer to the database
+        /// </summary>
+        /// <param name="p_cust">The customer to be added</param>
+        /// <returns>The customer that was added</returns>
         public Customer Add(Customer p_cust)
         {
             if(p_cust.Name == null || p_cust.Address == null || p_cust.Email == null || p_cust.PhoneNumber == null)
@@ -46,10 +60,20 @@ namespace BusinessLogic
             _data.AddCustomer(p_cust);
             return p_cust;
         }
+        /// <summary>
+        /// Gets all the orders made by a specific customer
+        /// </summary>
+        /// <param name="p_custId">The customer ID</param>
+        /// <returns>A list of orders or null if the customer ID is not found in the database</returns>
         public List<Order> GetOrdersByCustId(int p_custId)
         {
             return _data.GetOrdersByCustId(p_custId);
         }
+        /// <summary>
+        /// Checks to see if a customer exists in the database
+        /// </summary>
+        /// <param name="p_custId">The ID of the customer to search for</param>
+        /// <returns>True if the customer is found otherwise false</returns>
         public bool CustExists(int p_custId)
         {
             if (_data.GetCustomerByCustId(p_custId) != null)
