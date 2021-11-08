13c9bb61fa24f3c211601587c52ab6a5f98dfbb9
dlara2021 <dlara2021@gmail.com>
2021-11-08 17:57:58 -0500

    Adding Customer page

---
+++ StoreApp/WebUI/Controllers/CustomerController.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLogic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerBL _customerBL;
+        public CustomerController(CustomerBL p_customerBL)
+        {
+            _customerBL = p_customerBL;
+        }
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            return View(_customerBL.GetAll().Select(cust => new CustomerVM(cust)).ToList());
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StoreApp/WebUI/Models/CustomerVM.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class CustomerVM
+    {
+        public CustomerVM(Customer p_cust)
+        {
+            this.CustId = p_cust.CustID;
+            this.Name = p_cust.Name;
+            this.Address = p_cust.Address;
+            this.Email = p_cust.Email;
+            this.PhoneNumber = p_cust.PhoneNumber;
+        }
+        public int CustId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
